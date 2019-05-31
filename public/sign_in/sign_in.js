1c218a6a92e46251e802671303939cb555200764
aaanx <anna.walkowiakk@gmail.com>
2019-05-31 12:10:35 +0200

    Refractor firebase auth

--- public/sign_in/sign_in.js
+++ public/sign_in/sign_in.js
@@ -1,98 +1,75 @@
-// klik zaloguj -> pokazanie formularza logowania
-var zalogujBtn = document.getElementById('zaloguj_btn');
+// display log form
+const zalogujBtn = document.getElementById('zaloguj_btn');
 zalogujBtn.addEventListener('click', function() {
-    var logForm = document.querySelector('.zaloguj_form');
-    logForm.style.display === 'none' ? logForm.style.display = 'block' : logForm.style.display = 'none';
+  const logForm = document.querySelector('.zaloguj_form');
+  logForm.style.display === 'none' ? logForm.style.display = 'block' : logForm.style.display = 'none';
 });
 
-// klik button zaloguj się -> weryfikacja danych -> jeśli dane ok -> logowanie
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // user is logged in
+    
+    document.getElementById('wyloguj_btn').style.display = 'block';
+    const userDisplay = document.getElementById('username_display');
+    let userEmail = firebase.auth().currentUser.email;
+    userDisplay.innerHTML = userEmail;
+    
+    //document.querySelector('.log_info').textContent = '';
+    //document.querySelector('.zaloguj_form').style.display === 'none';
 
-function toggleSignIn() {
-    if (firebase.auth().currentUser) {
-        firebase.auth().signOut();
-    } else {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
+  } else {
+    // user is not logged in
+    document.getElementById('wyloguj_btn').style.display = 'none';
+  }
+});
 
-      if (email.length < 4) {
-        document.querySelector('.log_info').textContent = 'Please enter an email address';
-        return;
-      }
-      if (password.length < 4) {
-        document.querySelector('.log_info').textContent = 'Please enter a password at least 4 characters long';
-        return;
-      }        
+function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        location.reload(); 
-        // how to check every error without page reload
-        // error -> correct -> diff error -> correct -> diff error -> correct -> ok, log-in
+  if (password.length < 6) {
+    document.querySelector('.log_info').textContent = 'Please enter a password at least 6 characters long';
+    return;
+  } 
 
-        if (errorCode === 'auth/invalid-email') {
-            document.querySelector('.log_info').textContent = 'Wrong email format';
-            document.querySelector('.log_info').style.color = 'red';
-            console.log(errorMessage)
-            return;
-        } 
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+    
+    window.location = 'http://localhost:3000/';
+    
+  }).catch(function(error) {
+    
+    let errorCode = error.code;
+    let errorMessage = error.message;
 
-        if (errorCode === 'auth/user-not-found') {
-            document.querySelector('.log_info').textContent = 'User not found';
-            document.querySelector('.log_info').style.color = 'red';
-            console.log(errorMessage)
-            return;
-        } 
+    if (errorCode === 'auth/invalid-email') {
+      document.querySelector('.log_info').textContent = 'Wrong email format';
+      document.querySelector('.log_info').style.color = 'red';
+      console.log(errorMessage);
+      return;
+    } 
 
-        if (errorCode === 'auth/wrong-password') {
-            document.querySelector('.log_info').textContent = 'Wrong password';
-            document.querySelector('.log_info').style.color = 'red';
-            console.log(errorMessage)
-            return;
-        } 
+    if (errorCode === 'auth/user-not-found') {
+      document.querySelector('.log_info').textContent = 'User not found';
+      document.querySelector('.log_info').style.color = 'red';
+      console.log(errorMessage);
+      return;
+    } 
 
-        location.reload(); 
-      });
+    if (errorCode === 'auth/wrong-password') {
+      document.querySelector('.log_info').textContent = 'Wrong password';
+      document.querySelector('.log_info').style.color = 'red';
+      console.log(errorMessage);
+      return;
+    } 
 
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          let userEmail = firebase.auth().currentUser.email;
-          localStorage.setItem('user', userEmail);
-          console.log(userEmail);
-          document.getElementById('log_btn').disabled = false;
-          window.location = 'http://localhost:3000/';
-        } else {
-          document.getElementById('log_btn').disabled = true;
-          console.log('try again');
-          return false;
-        }
-      });
-      //document.getElementById('log_btn').addEventListener('click', enterTheGame, false);
-    }
+  });
 }
 
-function initApp() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-      } else {
-        // User is signed out.
-      }
-      document.getElementById('log_btn').disabled = false;
-    });
-    document.getElementById('log_btn').addEventListener('click', toggleSignIn, false);
+function logout() {
+  firebase.auth().signOut().then(function() {
+
+  }).catch(function(error) {
+    var errorMessage = error.message;
+    console.log(errorMessage);
+  });
 }
-
-window.onload = function() {
-    initApp();
-};
-
-// function enterTheGame() {
-//     location.replace("http://localhost:3000/");
-// }
--- public/sign_up/sign_up.js
+++ public/sign_up/sign_up.js
@@ -1,68 +1,49 @@
-// Kliknięcie button zarejestruj
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // user is logged in
+    } else {
+        // user is not logged in
+    }
+});
 
-function handleSignUp() {
-    var email = document.getElementById('register_email').value;
-    var passwordRepeat = document.getElementById('register_haslo2').value;
-    var password = document.getElementById('register_haslo').value;
+function signup() {
+    const email = document.getElementById('r_email').value;
+    const password = document.getElementById('r_password').value;    
+    const passwordRepeat = document.getElementById('r_password2').value;
 
     if (email.length < 6) {
-        document.querySelector('.register_info').textContent = 'Please enter an email address at least 6 characters long';
+        document.querySelector('.r_info').textContent = 'Please enter an email address at least 6 characters long';
         return;
     }
     if (password.length < 6) {
-        document.querySelector('.register_info').textContent = 'Please enter a password at least 6 characters long';
+        document.querySelector('.r_info').textContent = 'Please enter a password at least 6 characters long';
         return;
     }
     if (passwordRepeat !== password) {
-        document.querySelector('.register_info').textContent = 'Repeat the same password';
+        document.querySelector('.r_info').textContent = 'Repeat the same password';
         return;
     }
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+
+        document.querySelector('.r_info').textContent = 'Succesful registration!';
+        
+    }).catch(function(error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        console.log(errorMessage); 
+
         if (errorCode == 'auth/weak-password') {
-            document.querySelector('.register_info').textContent = 'The password is too weak';
+            document.querySelector('.r_info').textContent = 'The password is too weak';
             return;
         } 
         if (errorCode == 'auth/email-already-in-use') {
-            document.querySelector('.register_info').textContent = 'Email is already in use';
+            document.querySelector('.r_info').textContent = 'Email is already in use';
             return;
         } 
         if (errorCode == 'auth/invalid-email') {
-            document.querySelector('.register_info').textContent = 'Invalid email';
+            document.querySelector('.r_info').textContent = 'Invalid email';
             return;
         }
-
-        console.log(errorMessage); 
-        //console.log(error); 
     });
-
-    document.querySelector('.register_info').textContent = 'Succesful registration!';
 }
-
-function initApp() {
-    // Listening for auth state changes.
-    // [START authstatelistener]
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        console.log(displayName);
-        var email = user.email;
-        console.log(email);
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-      } else {
-        // User is signed out.
-      }
-    });
-    document.getElementById('register_btn').addEventListener('click', handleSignUp, false);
-}
-
-window.onload = function() {
-    initApp();
-};
